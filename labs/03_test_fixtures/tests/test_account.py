2ba2422bda460f1ba6b38306bc3efa9b4fbce0c1
Moussa TAMBA <tambakhi@gmail.com>
2025-04-25 11:09:14 +0000

    Update test_account.py

--- labs/03_test_fixtures/tests/test_account.py
+++ labs/03_test_fixtures/tests/test_account.py
@@ -25,6 +25,60 @@ class TestAccountModel(TestCase):
     def tearDown(self):
         """Remove the session"""
 
+
+
+# """
+# Test Cases TestAccountModel
+# """
+# import json
+# from unittest import TestCase
+# from models import db
+# from models.account import Account
+
+# ACCOUNT_DATA = {}
+
+# class TestAccountModel(TestCase):
+#     """Test Account Model"""
+
+#     @classmethod
+#     def setUpClass(cls):
+#         """ Connect and load data needed by tests """
+#         db.create_all()
+#         global ACCOUNT_DATA
+#         with open('tests/fixtures/account_data.json') as json_data:
+#             ACCOUNT_DATA = json.load(json_data)
+
+#     @classmethod
+#     def tearDownClass(cls):
+#         """Disconnect from database"""
+#         db.session.close()
+#     def setUp(self):
+#         """Truncate the tables"""
+#         db.session.query(Account).delete()
+#         db.session.commit()
+
+#     def tearDown(self):
+#         """Remove the session"""
+#         db.session.remove()
+
+#     ######################################################################
+#     #  T E S T   C A S E S
+#     ######################################################################
+#     def test_create_an_account(self):
+#         data = ACCOUNT_DATA[0]
+#         account = Account(**data)
+#         account.create()
+#         lendb = len(Account.all())
+#         self.assertEqual(lendb, 1)
+
+#     def test_create_all_accounts(self):
+        
+#         for data in ACCOUNT_DATA:
+#             account = Account(**data)
+#             account.create()
+#         lendb = len(ACCOUNT_DATA)
+#         self.assertEqual(len(Account.all()), lendb)
+
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
